ae6c750e0729a660e4fbf21c0ff5a376925f09b6
𝖒𝖚𝖘𝖙𝖆𝖋𝖆 <121257754+mustafaakalin@users.noreply.github.com>
2025-01-18 12:41:29 +0300

    Initialize C# project with .NET 8.0 framework and basic grading logic

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // öğrencinin notu 50 den küçükse kaldı 50 den büyük ise geçti , not 100 e eşit ise onur belgesi aldı.
+
+            git:
+            Console.WriteLine("Öğrencinin Notunu Giriniz");
+            byte not = Convert.ToByte(Console.ReadLine());
+            if (not < 50 && not > 0)
+            {
+                Console.WriteLine("Kaldı");
+            }
+            else if (not == 100)
+            {
+                Console.WriteLine("Onur Belgesi Aldı");
+            }
+            else if (not > 100 || not < 0)
+            {
+                Console.WriteLine("Hatalı Not Girişi 0-100 arasında bir not giriniz");
+                goto git;
+            }
+            else
+            {
+                Console.WriteLine("Geçti");
+            }
+        }
+    }
+}
